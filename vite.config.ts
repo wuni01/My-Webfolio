4bb824e63258ae36dedf14358e35a8f02651bdc8
wuni01 <94824952+wuni01@users.noreply.github.com>
2026-03-16 20:51:45 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/My-Webfolio/',
+    
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
